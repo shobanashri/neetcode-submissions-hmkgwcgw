1f176af8f63f6df0129f203262c6fab65e759825
Shobana shri J <136839404+shobanashri@users.noreply.github.com>
2026-04-06 11:21:29 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+
+        int n=arr.length;
+        int maxright = -1;
+
+        for(int i=n-1;i>=0;i--){
+            int current = arr[i];
+            arr[i]=maxright;
+
+            maxright = Math.max(current,maxright);
+        }
+        return arr;
+        
+    }
+}
