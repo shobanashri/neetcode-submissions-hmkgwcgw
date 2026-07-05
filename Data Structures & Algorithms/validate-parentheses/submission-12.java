7c774a79431905a7eb733a8800e28ab2b9580b2b
Shobana shri J <136839404+shobanashri@users.noreply.github.com>
2026-07-05 18:11:56 +0530

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='{'||s.charAt(i)=='['||s.charAt(i)=='('){
+                st.push(s.charAt(i));
+
+            }else if(!st.isEmpty() && (
+                (st.peek()=='{'&& s.charAt(i)=='}') ||
+                (st.peek()=='['&& s.charAt(i)==']') ||
+                (st.peek()=='('&& s.charAt(i)==')')
+            )){
+                st.pop();
+
+            }else{
+                st.push(s.charAt(i));
+
+            }
+            
+        }
+        if(!st.isEmpty()){
+                return false;
+
+        }
+        return true;
+        
+    }
+}
