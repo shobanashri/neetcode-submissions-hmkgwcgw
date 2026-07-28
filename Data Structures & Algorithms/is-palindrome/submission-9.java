da768628b0cac24a531825a61fa41fab418fdd15
Shobana shri J <136839404+shobanashri@users.noreply.github.com>
2026-07-28 21:40:04 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder str = new StringBuilder();
+
+        for(char ch:s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
+                str.append(Character.toLowerCase(ch));
+            }
+        }
+        return str.toString().equals(str.reverse().toString());
+        
+    }
+}
